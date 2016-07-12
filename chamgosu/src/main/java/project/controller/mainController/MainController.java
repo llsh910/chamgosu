17aa8d437b831f0ba4857d367bfe7dd4caf71869
llsh910 <llsh910@naver.com>
2016-07-12 16:45:13 +0900

    기본 프로젝트 설정

---
+++ chamgosu/src/main/java/project/controller/mainController/MainController.java
@@ -0,0 +1,25 @@
+package project.controller.mainController;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import project.config.common.CommandMap;
+
+@Controller
+public class MainController {
+	Logger log = Logger.getLogger(this.getClass());
+	
+	/*@Resource(name="projectService")
+	private ProjectService projectService;*/
+	
+	@RequestMapping(value="/main.do")
+	public ModelAndView projectList(CommandMap param) throws Exception{
+		ModelAndView mav = new ModelAndView("/main/main");
+		
+
+		return mav;
+	}
+	
+}
---
+++ chamgosu/src/main/webapp/WEB-INF/jsp/top.jsp
@@ -0,0 +1,6 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page import="java.util.*, java.util.Date, java.text.*, java.io.*, java.net.*, project.config.util.*"%>
+<%@ include file="url.jsp" %>
+<%
+	//세션처리
+%>
