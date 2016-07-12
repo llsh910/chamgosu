89717885ef8dbf34e34266c701fd919219ebb5c9
llsh910 <llsh910@naver.com>
2016-07-12 18:50:49 +0900

    commit code

--- chamgosu/src/main/java/project/dao/code/CodeDAO.java
+++ chamgosu/src/main/java/project/dao/code/CodeDAO.java
@@ -29,6 +29,25 @@ public class CodeDAO extends TempletDAO {
 		return (Map<String, Object>)selectOne("code.code1Value", map);
 	}
 	
+	@SuppressWarnings("unchecked")
+	public Map<String, Object> code2Value(Map<String, Object> map) throws Exception{
+		return (Map<String, Object>)selectOne("code.code2Value", map);
+	}
 	
+	public int code1Save(Map<String, Object> map) throws Exception{
+		return (Integer)insert("code.code1Save", map);
+	}
+	
+	public int code2Save(Map<String, Object> map) throws Exception{
+		return (Integer)insert("code.code2Save", map);
+	}
+	
+	public int code1ModifySave(Map<String, Object> map) throws Exception{
+		return (Integer)update("code.code1ModifySave", map);
+	}
+	
+	public int code2ModifySave(Map<String, Object> map) throws Exception{
+		return (Integer)update("code.code2ModifySave", map);
+	}
 	
 }
